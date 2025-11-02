839b8d123a843bfde5ec13201f83eb751d983943
Luiz-Farias-dev <luizferreirafariasn@gmail.com>
2025-11-02 12:40:29 -0300

    Criando controller de users

---
+++ src/main/java/com/learning/agregador/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.learning.agregador.controller;
+
+import com.learning.agregador.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody String body) {
+        //
+        return null;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
+        //
+        return null;
+    }
+}
